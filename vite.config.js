d9b7e5cbb5aa43ce43aaa0ce207c3c115743e698
Bhautik Godhasara <118058581+Bhautik204@users.noreply.github.com>
2025-05-13 14:26:15 +0530

    Initial Build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,27 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example: Separate vendor libraries into their own chunks
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) {
+              return 'vendor_vue';
+            }
+            if (id.includes('chart.js')) {
+              return 'vendor_chartjs';
+            }
+            // Add more conditions as needed for other libraries
+            return 'vendor';
+          }
+        },
+      },
+    },
+    // Optional: Increase the chunk size warning limit if necessary
+    chunkSizeWarningLimit: 1000, // in KB
+  },
   server: {
     port: 3000,
     proxy: {
