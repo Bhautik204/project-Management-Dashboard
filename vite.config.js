de1d3a1b89ca57f4b798c9b34b6dd658152cd45c
Bhautik Godhasara <118058581+Bhautik204@users.noreply.github.com>
2025-05-13 11:34:09 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,23 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+ build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
+            if (id.includes('chart.js')) {
+              return 'chart-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
